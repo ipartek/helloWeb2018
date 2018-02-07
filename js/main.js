9c83c1befda022607007ed5a6d4ee91c8ae83c04
alexanderDevWeb <alexanderbartolomesan@gmail.com>
2018-02-07 20:46:41 +0100

    commit_casa

--- js/main.js
+++ js/main.js
@@ -1,76 +1,133 @@
-/* JavaScript */
+/*  JavaScript */
 
+/* Ejecutamos esta función al cargar la página web */
 
-//Ejecutamos esta funcion al cargar la pagina Web
-function init(){
-
+function init() {
     console.info('DOM listo y cargado');
-
 }
 
+function pulsador() {
+    console.info("Botón pulsado");
 
-function pulsador(){
+    var texto = document.getElementById("texto");
 
-    console.info('Boton pulsado');
+    texto.textContent = "Pulsado!";
 
-    var texto  = document.getElementById("texto");
     var numero = document.getElementById("numero").value;
-    var mensaje = "";
 
-    if ( "" == numero ){
-       mensaje = "No me seas vago y dime un numero ;-)";
-    }else{
+    console.info(numero);
 
-        if ( numero % 2 == 0 ){
-            mensaje = numero + " es PAR";
-        }else{
-            mensaje = numero + " es IMPAR";
+
+
+    if (!numero) {
+        texto.textContent = "Debe introducir un número";
+    } else {
+        if (numero % 2 == 0 || numero == 0) {
+            texto.textContent = "El número " + numero + " es par!";
+            texto.style.color = "green";
+        } else {
+            texto.textContent = "El número " + numero + " es impar!";
+            texto.style.color = "blue";
         }
-
     }
-
-    texto.textContent = mensaje;
-
 }
-
 
 /**
-*   llamada mediante Ajax para conseguir el precio actual de un Bitcoin
-*   @see: https://www.coindesk.com/api/
-*/
-function llamadaApi(){
+ *
+ *Lllamada mediante Ajax para conseguir el precio actual de un Bitcoin
+ *@see: https://www.coindesk.com/api/
+ *
+ *
+ **/
+
+function llamadaApi() {
+    console.info("llamada Api");
 
     var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
-    var moneda = document.getElementById("moneda").value;
-    //console.debug('Moneda seleccionada %s', moneda );
 
-    //lamada Ajax
-     var xhr = new XMLHttpRequest();
-     xhr.onreadystatechange = function() {
-        //esperamos a que la respuesta sea correcta
+    /*llamada AJAX*/
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        //Esperamos a que la terpsuesta sea correcta
         if (this.readyState == 4 && this.status == 200) {
-           var json = JSON.parse(this.responseText);
+            var json = JSON.parse(this.responseText);
+            console.info(json.bpi);
+            var precio = json.bpi;
 
-           //console.info(json.bpi.USD.rate);
+            var moneda = document.getElementById("moneda").value;
 
-           //mostrar en pantalla
-           var precio = "";
-           if ( moneda == "USD"){
-              precio = json.bpi.USD.rate + " Dolares";
-           }else if (moneda == "EUR"){
-                precio = json.bpi.EUR.rate + " Euros";
-           }else{
-                precio = json.bpi.GBP.rate + " Libras";
-           }
-
-
-           document.getElementById("precio").textContent =  precio;
-
-
+            switch (moneda) {
+                case "EUR":
+                    document.getElementById("precio").innerHTML = precio.EUR.rate + " €";
+                    break;
+                case "USD":
+                    document.getElementById("precio").innerHTML = precio.USD.rate + " $";
+                    break;
+                case "GBP":
+                    document.getElementById("precio").innerHTML = precio.GBP.rate + " &pound";
+                    break;
+            }
         }
-     };
-     xhr.open("GET", url , true);
-     xhr.send();
-
+    };
+    xhr.open("GET", url, true);
+    xhr.send();
 }
 
+
+/* Sorteo sobre los alumnos */
+/* Declaro fuera el array para evitar el undefined */
+var arrResultados = new Array();
+
+function sorteo() {
+
+    //Recojo el array de localstorage en el caso de que ya exista
+    if (localStorage.getItem('sorteo')) {
+        console.info("Cookie ya existente");
+
+        //Cargo el contenido de la cookie en el array
+        arrResultados = JSON.parse(localStorage.getItem("sorteo"));
+
+        //Si ya está completo lo reinicio
+        if(arrResultados.length == 15)
+            arrResultados = [];
+
+        console.info("Su valor actual es: " + arrResultados);
+
+    } else {
+        console.info("Cookie NO existente");
+    }
+
+    var random = Math.floor(Math.random() * 15);
+    console.info("Número random: " + random);
+
+
+    while (arrResultados.indexOf(random) != -1) {
+        console.info("while, random ya existente");
+        var random = Math.floor(Math.random() * 15);
+        console.info("Número random: " + random);
+    }
+
+    arrResultados.push(random);
+
+    //Guardo el array en una localstorage
+    localStorage.setItem("sorteo", JSON.stringify(arrResultados));
+
+    //Pongo las celdas reales del numero recogido al azar
+    var arrCeldas = [
+        [7,0], [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0],
+        [0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7,2]
+    ];
+
+    //Informo de la columna y celda recogida
+    console.info("Columna: " + arrCeldas[random][0] + " Celda: " + arrCeldas[random][1])
+
+    //Pinto el fondo de la celda seleccionada y muestro el lector
+    document.getElementById("alumnos").rows[arrCeldas[random][0]].cells[arrCeldas[random][1]].style.backgroundColor = "#FF4C4C";
+    document.getElementById("lector").textContent = document.getElementById("alumnos").rows[arrCeldas[random][0]].cells[arrCeldas[random][1]].innerHTML;
+
+    //Oculto el botón de Sortear y muyestro el de reset
+    document.getElementById("btnsort").style.display = "none";
+    document.getElementById("btnreset").style.display = "block";
+
+    console.info(document.getElementsByTagName("td")[0].textContent);
+}

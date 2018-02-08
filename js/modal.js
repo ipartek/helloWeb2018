93333167898f9fc8aa851dadbc5f3ee89e93d7e8
Curso <Curso@IPARTEKAULA.COM>
2018-02-08 14:02:35 +0100

    Bootstrap

--- js/modal.js
+++ js/modal.js
@@ -2,19 +2,22 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('imagen');
+var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
+
+img.onclick = function () {
     modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+    //modalImg.src = this.src;
+    captionText.innerHTML = modalImg.alt;
 }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function (event) {
+    event.stopPropagation();
+    console.debug("cerramos");
+    modal.style.display = "none";
 }

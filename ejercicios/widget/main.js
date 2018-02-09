822316b1b53c810c4822c140ecd9439be9afc463
Aingeru Sanchez <Aingeru1703@gmail.com>
2018-02-09 23:38:13 +0100

    NEW Weather Widget en ejercicios/widget

--- ejercicios/widget/main.js
+++ ejercicios/widget/main.js
@@ -3,6 +3,15 @@
     API Key para las peticiones JSON: http://api.openweathermap.org/data/2.5/weather?q=Bilbao&APPID=bd5e378503939ddaee76f12ad7a97608
 */
 
+// TODO: Método para lanzar verTiempo() al pulsar ENTER sobre el button
+function pulsarEnter(e) {
+    var char = e.which || e.keyCode;
+    if(char == "13") {
+        verTiempo();
+    }
+}
+
+
 // Función para hacer la llamada a la API openweathermap
 function verTiempo() {
     // Variables de UI
@@ -11,22 +20,30 @@ function verTiempo() {
     // Comprobar que ha introducido texto
     if (ciudad == undefined || ciudad == "") {
         mensaje.innerHTML = "No ha introducido la ciudad";
-        console.debug("No se ha introducido la ciudad");
+        console.debug("No ha introducido la ciudad");
     } else {
         mensaje.innerHTML = "Obtener el tiempo de la ciudad " + ciudad;
         console.debug("Obtener el tiempo de la ciudad: %s", ciudad);
 
-        // TODO: formar la URL con la ciudad introducida
-        var url = "http://api.openweathermap.org/data/2.5/weather?q=Bilbao&APPID=bd5e378503939ddaee76f12ad7a97608";
+        // TODO[x]: formar la URL con la ciudad introducida
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&APPID=bd5e378503939ddaee76f12ad7a97608";
+        console.debug("URL: " + url);
 
         // Petición AJAX a la API openweathermap
         var xhr = new XMLHttpRequest();
+        var encontrado = null;
         xhr.onreadystatechange = function () {
             // verificar que la respuesta sea correcta
             if (this.readyState == 4 && this.status == 200) {
                 var json = JSON.parse(this.responseText);
                 console.debug("json %o", json);
-                actualizarWidget(json);
+                encontrado = true;
+                actualizarWidget(json, encontrado);
+            } else if (this.status == 404) {
+                mensaje.innerHTML = "No se encuentra la ciudad: " + ciudad;
+                console.debug("No se encuentra la ciudad: " + ciudad);
+                encontrado = false;
+                actualizarWidget(json, encontrado);
             }
         };
         xhr.open("GET", url, true);
@@ -34,14 +51,28 @@ function verTiempo() {
     }
 }
 
-function actualizarWidget(json) {
-    document.getElementById("city").textContent = json.name;
-    // TODO[x]: cambiar de Kelvin a Celsius
-    document.getElementById("temp").textContent = Math.floor(json.main.temp - 270) + "º";
-    // Obtener el icono de OpenWeatherMap (OWM)
-    var iconOWM = json.weather[0].icon;
-    // TODO: Colocar icono
-    // @see: https://openweathermap.org/weather-conditions
-    document.getElementById("forecastIcon");
-
+function actualizarWidget(json, encontrado) {
+    var temperatura = null;
+    var ciudad = null;
+    var iconoPrevision = null;
+    if (encontrado) {
+        ciudad = json.name;
+        // TODO[x]: cambiar de Kelvin a Celsius
+        temperatura = Math.round(json.main.temp - 270) + "º";
+        // Obtener el icono de OpenWeatherMap (OWM)
+        var idIcono = json.weather[0].icon;
+        // TODO[x]: Colocar icono
+        // @see: https://openweathermap.org/weather-conditions
+        var icono =  // Elemento del icono en el HTML
+        // Conseguir el icono de openweathermap = "http://openweathermap.org/img/w/ + idIcono + ".png"
+        iconoPrevision = "http://openweathermap.org/img/w/" + idIcono + ".png";
+        console.debug("URL Icono: http://openweathermap.org/img/w/" + idIcono + ".png");
+    } else {
+        temperatura = "404";
+        ciudad = "Not Found"
+        iconoPrevision = "http://openweathermap.org/img/w/50d.png" ;
+    }
+    document.getElementById("temp").textContent = temperatura;
+    document.getElementById("city").textContent = ciudad;
+    document.getElementById("owmIcon").src = iconoPrevision;
 }

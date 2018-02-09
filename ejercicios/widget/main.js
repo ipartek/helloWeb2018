5945624b8baa9d48bec4cb1734ce20d050ee467b
anderuraga <ander.uraga@gmail.com>
2018-02-09 14:03:43 +0100

    ejercicio widget tiempo medio mascado

--- ejercicios/widget/main.js
+++ ejercicios/widget/main.js
@@ -1,1 +1,41 @@
-// http://api.openweathermap.org/data/2.5/weather?q=London&APPID=bd5e378503939ddaee76f12ad7a97608
+var url = "http://api.openweathermap.org/data/2.5/weather?q=Bilbao&APPID=bd5e378503939ddaee76f12ad7a97608"
+
+
+function verTiempo() {
+    var ciudad = document.getElementById("input_ciudad").value;
+    console.debug('Quiero ver el tiempo de %s', ciudad);
+
+    var mensaje = document.getElementById("mensaje");
+
+    if (ciudad == "") {
+        mensaje.textContent = "Yo no conocer tu Ciudad";
+    } else {
+        mensaje.textContent = "Realizando peticion....";
+
+        //TODO poner en la URL la ciudad
+
+        // llamada Ajax/////////////////////////////////////////
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            //esperamos a que la respuesta sea correcta
+            if (this.readyState == 4 && this.status == 200) {
+                var json = JSON.parse(this.responseText);
+                console.debug("json %o", json);
+                rellenarWidget(json);
+            }
+        };
+        xhr.open("GET", url, true);
+        xhr.send();
+    }
+}
+
+
+function rellenarWidget(json){
+
+    document.getElementById("city").textContent = json.name;
+    //TODO cambiar de Kelvins a Celsius
+    document.getElementById("temp").textContent = json.main.temp;
+
+
+
+}

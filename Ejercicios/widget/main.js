2a94186dd1af2f4ef2ca1f778e05a3b7a9b8356e
joseba10 <>
2018-02-11 22:59:16 +0100

    Ejercicio de pintxos corregido,ejercicio del tiempo mejorado,falta poner la imagen del dia que hace

--- Ejercicios/widget/main.js
+++ Ejercicios/widget/main.js
@@ -1,5 +1,5 @@
 //http://api.openweathermap.org/data/2.5/weather?q=Bilbao&APPID=bd5e378503939ddaee76f12ad7a97608
- var url = "http://api.openweathermap.org/data/2.5/weather?q=Bilbao&APPID=bd5e378503939ddaee76f12ad7a97608";
+
 
 function ver_Tiempo(){
 
@@ -19,8 +19,10 @@ function ver_Tiempo(){
 
         mensaje.textContent="Realizando Peticion";
 
+       var url = "http://api.openweathermap.org/data/2.5/weather?q=";
+        var url2 = "&APPID=bd5e378503939ddaee76f12ad7a97608";
 
-
+        url = url + ciudad + url2;
 
     //llamada Ajax
      var xhr = new XMLHttpRequest();
@@ -56,15 +58,21 @@ function ver_Tiempo(){
 
 function rellenarWidget(json){
 
-    document.getElementById("city").textContent=json.name;
-    var temperatura=document.getElementById("temp").textContent=json.main.temp;
+    document.getElementById("city").textContent=json.name; /*Cogemos el valor de la ciudad y la añadimos*/
 
-    temperatura_celsius= (temperatura -273.15);
-    temperatura_real=Math.round(temperatura_celsius);
 
-    document.getElementById("temp").textContent=temperatura_real + "º";
+    var temperatura=textContent=json.main.temp; //Recoge el valor de la API//
+    temperatura_celsius= (temperatura -273.15); //Pasa la temperatura Fahrenheit  a Grados Celsius//
+    temperatura_real=Math.round(temperatura_celsius); //Una vez hecho la conversion,redondeamos esa temperatura//
 
-    imagen= document.getElementById("icono").textContent=json.weather.icon;
+    document.getElementById("temp").textContent=temperatura_real + "º"; //Añadimos el contenido
+
+
+    imagen=textContent=json.weather.icon;
+    console.log(imagen);/*Cogemos el valor del icono de la temperatura*/
+
+
+
 }
 
 

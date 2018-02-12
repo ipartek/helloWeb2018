28f6ddaec205fcf08ae68529e62a1fb2468fbbc3
Curso <Curso@IPARTEKAULA.COM>
2018-02-12 12:41:06 +0100

    form

--- ejemplos/helloWeb_old/js/main.js
+++ ejemplos/helloWeb_old/js/main.js
@@ -90,7 +90,16 @@ function llamadaApi(){
 
     }
 
-}
 
+}
+function toggler(e) {
+        if( e.innerHTML == 'Show' ) {
+            e.innerHTML = 'Hide'
+            document.getElementById('password').type="text";
+        } else {
+            e.innerHTML = 'Show'
+            document.getElementById('password').type="password";
+        }
+}
 
 
--- ejemplos/widget/main.js
+++ ejemplos/widget/main.js
@@ -1,7 +1,7 @@
-var url= "http://api.openweathermap.org/data/2.5/weather?q=Bilbao&APPID=bd5e378503939ddaee76f12ad7a97608";
+
 
 function ver_Tiempo(){
-    var ciudad = document.getElementById("input_ciudad").value;
+    var ciudad = document.getElementById("input_ciudad").value.toString();
     console.debug('Quiero ver el tiempo de %s', ciudad);
 
     var mensaje = document.getElementById("mensaje");
@@ -14,9 +14,11 @@ function ver_Tiempo(){
     }
 
 
+var url1= "http://api.openweathermap.org/data/2.5/weather?q=";
 
 
-
+var url3 = ciudad + "&APPID=bd5e378503939ddaee76f12ad7a97608";
+var url=  url1 + url3;
        var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             //esperamos a que la respuesta sea correcta
@@ -33,12 +35,24 @@ function ver_Tiempo(){
 
 function rellenarWidget(json){
 
+   var icono = document.getElementById("icono");
+   icono.setAttribute("data-icon","B");
+   var descripcion= json.weather[0].description
+   if(descripcion == "light rain")
+       icono.setAttribute("data-icon","C");
+    else
+        icono.setAttribute("data-icon","B");
     document.getElementById("city").textContent = json.name;
     //TODO cambiar de Kelvins a Celsius
-    document.getElementById("temp").textContent = json.main.temp;
+  //  document.getElementById("temp").textContent = json.main.temp;
+    kelvin = json.main.temp;
+    celsius = kelvin - 273;
+    celsius = celsius.toFixed(1);
+    document.getElementById("temp").textContent = celsius + "ºC";
 
 
 
 }
 
 
+;

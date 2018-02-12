965d9c489f2b690b38987773e5b9f5d129f0edcc
Curso <Curso@IPARTEKAULA.COM>
2018-02-12 14:29:50 +0100

    OTS12

--- ejercicios/Widget/main.js
+++ ejercicios/Widget/main.js
@@ -1,20 +1,23 @@
-var url = "http://api.openweathermap.org/data/2.5/weather?q=Bilbao&APPID=bd5e378503939ddaee76f12ad7a97608"
+// http://api.openweathermap.org/data/2.5/weather?q=London&APPID=bd5e378503939ddaee76f12ad7a97608
 
 
-function ver_tiempo() {
-    var ciudad = document.getElementById("input_ciudad").value;
+
+var url = "http://api.openweathermap.org/data/2.5/weather?q=##CITY##&APPID=bd5e378503939ddaee76f12ad7a97608"
+
+function verTiempo() {
+     var ciudad = document.getElementById("input_ciudad").value;
     console.debug('Quiero ver el tiempo de %s', ciudad);
 
     var mensaje = document.getElementById("mensaje");
-
     if (ciudad == "") {
-        mensaje.textContent = "Yo no conocer tu Ciudad";
+        mensaje.textContent = "No has introducido una ciudad";
     } else {
-        mensaje.textContent = "Realizando peticion....";
+         mensaje.textContent = "Realizando peticion....";
 
-        //TODO poner en la URL la ciudad
+        //poner en la URL la ciudad
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + ciudad + "&APPID=bd5e378503939ddaee76f12ad7a97608";
 
-        // llamada Ajax/////////////////////////////////////////
+        //llamada Ajax
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             //esperamos a que la respuesta sea correcta
@@ -22,20 +25,30 @@ function ver_tiempo() {
                 var json = JSON.parse(this.responseText);
                 console.debug("json %o", json);
                 rellenarWidget(json);
+                 /* mensaje.textContent = "El tiempo en " + ciudad;
+                rellenarWidget(json);*/
+
+            } else if (this.readyState == 4 && this.status == 404) {
+                mensaje.textContent = "La ciudad que desea consultar no exiate";
             }
         };
         xhr.open("GET", url, true);
         xhr.send();
+
     }
 }
 
-
-function rellenarWidget(json){
+function rellenarWidget(json) {
 
     document.getElementById("city").textContent = json.name;
-    //TODO cambiar de Kelvins a Celsius
-    document.getElementById("temp").textContent = json.main.temp;
 
+   //cambiar de Kelvins a Celsius
+    var tempCelsius = parseInt(json.main.temp) - 273;
+    document.getElementById("temp").textContent = tempCelsius + "º";
 
+    //cambiar imagen icono
+    var icon = json.weather[0].icon;
+    document.getElementById("icon").src = iconUrl + icon + ".png";
 
-}
+    }
+

f008529a386c5079223570d6fa2fe90cd73a7a00
Curso <Curso@IPARTEKAULA.COM>
2018-02-12 14:22:01 +0100

    12f

--- Ejercicios/widget/main.js
+++ Ejercicios/widget/main.js
@@ -1,4 +1,4 @@
-var url = "http://api.openweathermap.org/data/2.5/weather?q=Bilbao&APPID=bd5e378503939ddaee76f12ad7a97608"
+var url = "http://api.openweathermap.org/data/2.5/weather?q=##CITY##&APPID=bd5e378503939ddaee76f12ad7a97608"
 
 
 function verTiempo(){
@@ -12,21 +12,24 @@ function verTiempo(){
     }else{
     mensaje.textContent = "Realizando peticion...";
 
+        url = url.replace("##CITY##", ciudad);
+
         // Llamada Ajax /////////////////////
         var xhr = new XMLHttpRequest();
-
         xhr.onreadystatechange = function() {
 
         //esperamos a que la respuesta sea correcta
 
         if (this.readyState == 4 && this.status == 200){
-
            var json = JSON.parse(this.responseText);
-
             console.debug("json %o", json);
-
             rellenarWidget(json);
             }
+
+            if (this.readyState == 4 && this.status == 404){
+           mensaje.textContent = "No exista esa ciudad";
+            }
+
         };
 
         xhr.open("GET", url, true);
@@ -37,9 +40,20 @@ function verTiempo(){
 
 function rellenarWidget(json){
 
+    var iconUrl = "http://openweathermap.org/img/w/"; //09.png
+
     document.getElementById("city").textContent = json.name;
     //TODO cambiar de Kelvins a Celsius
-    document.getElementById("temp").textContent = json.main.temp;
+
+    var tempCelsius = parseInt(json.main.temp) -273;
+
+    document.getElementById("temp").textContent = tempCelsius + "º";
+
+
+    // cambiar imagen icono
+
+    var icon = json.weather[0].icon
+    document.getElementById("icon").src = iconUrl + icon + ".png";
 
 
 

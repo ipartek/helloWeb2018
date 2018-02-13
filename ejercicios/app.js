35efe6cb8c8e2431e3b84132b00e223f592bcbc1
Curso <Curso@IPARTEKAULA.COM>
2018-02-13 12:41:38 +0100

    Nodejs cargado

--- ejercicios/app.js
+++ ejercicios/app.js
@@ -1,7 +1,19 @@
 /* ejemplo para levantar un servidor web con NODEJS*/
 var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
+var url =require('url');
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    var parametros=url.parse(request.url, true).query;
+    //recibir parámetro por Get
+    //ejemplo de uso:
+    //http://localhost:8084/?nombre=Inigo&apellido=Gallego&edad=24
+    //Respuesta para el navegador
+    response.write('<h1>Servidor escuchando :D</h1>');
+    response.write('<p>Nombre:</p>');
+    response.write('<p>'+parametros.nombre+'</p>');
+    response.write('<p>Apellido:</p>');
+    response.write('<p>'+parametros.apellido+'</p>');
+    response.write('<p>Edad:</p>');
+    response.write('<p>'+parametros.edad+'</p>');
+    response.end();
+}).listen(8084);

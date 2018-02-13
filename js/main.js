dfb0c56382d81c5671bf4433b88a369e56d9a41e
Curso <Curso@IPARTEKAULA.COM>
2018-02-13 14:32:04 +0100

    Ordenacionenburbuja hecho

--- js/main.js
+++ js/main.js
@@ -4,7 +4,12 @@
 //Ejecutamos esta función al cargar la página web
 
 function init() {
-    console.info('DOM listo y cargado');
+    //5 niveles de logs otrazas ordenados por prioridad
+    console.debug('debug DOM listo y cargado');
+    console.log('logDOM listo y cargado');
+    console.info('info DOM listo y cargado');
+    console.warn('warn DOM listo y cargado');
+    console.error('error DOM listo y cargado');
 }
 
 function pulsador(){
@@ -62,4 +67,25 @@ function llamadaApi(){
   xhr.open("GET", url, true);
   xhr.send();
 }
+function ordenacionEnBurbuja(){
+    var numerosDesordenados=[3,0,1,8,7,2,5,4,6,9];
+    var noHayCambios=false;
+    var aux=0;
+    var numero1;
+    var numero2;
+    while(noHayCambios==false){
+        noHayCambios=true;
+        for(var i=0;i==numerosDesordenados.length-1;i++){
+            numero1=numerosDesordenados[i];
+            numero2=numerosDesordenados[i+1];
+            if(numero1>numero2){
+                noHayCambios=false;
+                aux=numero2;
+                numero2=numero1;
+                numero1=aux;
+            }
+        }
 
+    }
+
+}

b97b2ff19ae7bcd622d8e68419dafc1daa1a8b61
Curso <Curso@IPARTEKAULA.COM>
2018-02-15 14:25:06 +0100

    Metodos de location agregados, Filtro de edad mejorado,

--- Ejercicios/javascript/calcular.js
+++ Ejercicios/javascript/calcular.js
@@ -5,7 +5,32 @@ function calcular() {
     var numero1 = prompt("Dime un numero");
     var numero2 = prompt("Dime otro numero");
 
-    if (numero1 == null) {
+
+    do {
+        alert("No puedo calcular");
+        numero1 = prompt("Dime un numero");
+    } while (numero1 == null || numero1 = "");
+
+    do {
+        alert("No puedo calcular");
+        numero2 = prompt("Dime otro numero");
+    } while (numero1 == null || numero1 = "");
+
+    do {
+        var calcular = parseInt(numero1) + parseInt(numero2);
+
+
+
+        document.getElementById("caja").innerHTML = calcular;
+
+
+
+    }
+    while (numero1 != false && numero2 != false)
+
+
+
+    /* if (numero1 == null) {
 
         alert("no puedo calcular");
 
@@ -29,18 +54,18 @@ function calcular() {
 }
 
 
+*/
+
+    function validaredad() {
+
+        var edad = confirm("Eres menor o mayor de edad");
+
+        if (edad == false) {
+
+            document.location.replace('menor.html');
 
 
-function validaredad() {
 
-     var edad = prompt("Eres menor o mayor de edad");
-
-    if (edad<18) {
-
-       document.location.replace('menor.html');
-
-
+        }
 
     }
-
-}
---
+++ Ejercicios/javascript/imagen.js
@@ -0,0 +1,9 @@
+
+var host=document.location.host;
+var port=document.location.port;
+var protocolo=document.location.protocol;
+
+
+console.log(" HOST "+ host + " Puerto " + port + " Protocolo " + protocolo);
+
+

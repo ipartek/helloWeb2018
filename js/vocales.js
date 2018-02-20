f10d8f9a5cb8fdd903ee053bbcb37e91abbc0c51
Curso <Curso@IPARTEKAULA.COM>
2018-02-20 14:28:24 +0100

    Falta hacer los remove event liseners

--- js/vocales.js
+++ js/vocales.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var btnReordenar = document.getElementById('btnReordenar');
 var vocales = ['vocalA', 'vocalE', 'vocalI', 'vocalO', 'vocalU'];
 
+/* reodena los contenedores de las vocales usando order de flex */
 function reordenarVocales() {
 
     console.log(vocales);
@@ -25,19 +28,106 @@ function reordenarVocales() {
 
 btnReordenar.addEventListener('click', function () {
     reordenarVocales();
-
+    console.log('reordenar');
 }, false)
 
 
 var parrafos = document.getElementsByTagName('p');
-
-for (i = 0; i < parrafos.length; i++) {
+/*pinta de rojo el parrafo seccionado */
+for (let i = 0; i < parrafos.length; i++) {
 
     parrafos[i].addEventListener('click', function (event) {
-        for (i = 0; i < parrafos.length; i++) {
+        //reset
+        for (let i = 0; i < parrafos.length; i++) {
             parrafos[i].style.backgroundColor = 'white';
         }
-        console.log('clickiti');
+        //console.log('clickiti');
+        //pintar el parrafo
         event.target.style.backgroundColor = 'red';
     }, false);
+}
+
+//añade los eventlisteners para el sonido
+//habilitar();
+
+function sonido(filename) {
+
+    var audio = new Audio('../../sounds/' + filename + '.wav');
+
+    audio.play();
+
+    let id = 'vocal' + filename.toUpperCase();
+
+    let divNaranja = document.getElementById(id);
+    divNaranja.style.backgroundColor = 'orange';
+
+    console.log('audio' + filename);
+    deshabilitar(filename);
+
+    audio.addEventListener('ended', function () {
+        habilitar();
+    })
+}
+
+
+function deshabilitar(vocal) {
+
+    let id = 'vocal' + vocal.toUpperCase();
+
+
+
+    for (let i = 0; i < vocales.length; i++) {
+        let letra = vocales[i].slice(-1).toLowerCase();
+        let vFunction = function () {
+            sonido(letra);
+        };
+
+        console.log('letra ' + letra);
+        document.getElementById(vocales[i]).removeEventListener('click', vFunction, false);
+
+        document.getElementById(vocales[i]).style.backgroundColor = 'black';
+        //console.log('disabled'+vocales[i]);
+
+    }
+
+    let divNaranja = document.getElementById(id);
+    divNaranja.style.backgroundColor = 'orange';
+
+}
+
+
+
+function habilitar() {
+
+
+
+    for (let i = 0; i < vocales.length; i++) {
+        let vFunction;
+        let letra;
+        letra = vocales[i].slice(-1).toLowerCase();
+        console.log('letra ' + letra);
+
+        vFunction = function () {
+            sonido(letra);
+        };
+
+        document.getElementById(vocales[i]).addEventListener('click', vFunction, false);
+        //console.log('disabled'+vocales[i]);
+
+    }
+
+}
+
+function habilitarA() {
+        let vFunction;
+        let letra;
+        letra = 'a';
+        console.log('letra ' + letra);
+
+        vFunction = function () {
+            sonido(letra);
+        };
+
+        document.getElementById('vocalA').addEventListener('click', vFunction, false);
+        //console.log('disabled'+vocales[i]);
 }

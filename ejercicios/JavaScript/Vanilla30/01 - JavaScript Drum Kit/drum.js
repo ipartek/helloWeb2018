7924a64b746f1f365a2b7c931c35b66898559b93
mikelaranburu <aranburu89@gmail.com>
2018-02-21 21:56:48 +0100

    OTS21_tx

--- ejercicios/JavaScript/Vanilla30/01 - JavaScript Drum Kit/drum.js
+++ ejercicios/JavaScript/Vanilla30/01 - JavaScript Drum Kit/drum.js
@@ -1,20 +1,20 @@
-var keys = document.querySelectorAll(".key");
 
-function playSound(e) {
-  
-    var audio = document.querySelector('audio[data-key="' + key + '"]');
-    var key = document.querySelector('.key[data-key="${e.keyCode}"]');
-    if (!audio) return; //para la funcion para que no suenen a la vez
-    audio.currentTime = 0; //revovina al principio
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
     audio.play();
-    key.classList.add("playing");
+  }
 
-    function removeTransition(e) {
-        if (e.propertyName !== 'transform') return; //skip si no es un transform
-        this.classList.remove('playing');
-    }
-
-    const keys = document.querySelectorAll('.key');
-    keys.forEach(key => key.addEventListener('transitioned', removeTransition))
-}
-window.addEventListener('keydown', playSound);
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);

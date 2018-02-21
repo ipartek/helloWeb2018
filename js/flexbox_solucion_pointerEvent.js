0636c2e732e6257f0422d340113e3439c6a3fe22
Curso <Curso@IPARTEKAULA.COM>
2018-02-21 10:59:27 +0100

    flexbox v1.2 210218

--- js/flexbox_solucion_pointerEvent.js
+++ js/flexbox_solucion_pointerEvent.js
@@ -1,8 +1,4 @@
 "use strict";
-
-
-
-
 var aOrden = [1, 2, 3, 4, 5];
 var aSonidos = ['a', 'e', 'i', 'o', 'u'];
 
@@ -22,28 +18,7 @@ btn.addEventListener('click', function () {
     }
 });
 
-function sonido() {
-    var letra = event.target.parentElement.dataset.letter;
-    event.target.parentElement.style.backgroundColor = 'orange';
-    console.debug('letra %s', letra);
 
-    for (var i = 0; i < boxes.length; i++) {
-        boxes[i].removeEventListener('click', sonido);
-    }
-
-    var audio = new Audio('../sounds/' + letra + '.wav');
-    audio.play();
-    audio.onended = habilitar;
-
-}
-
-function habilitar() {
-    //volver habilitar botones
-    for (var i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener('click', sonido);
-        boxes[i].style.backgroundColor = 'teal';
-    }
-}
 
 
 /**
@@ -59,4 +34,28 @@ function shuffle(a) {
         a[j] = x;
     }
     return a;
+}
+
+
+
+var contentLetras = document.getElementById("container-flex");
+
+var boxes = document.getElementsByClassName("box");
+for (var i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', sonido);
+}
+
+function sonido() {
+    var letra = event.target.parentElement.dataset.letter;
+    event.target.parentElement.style.backgroundColor = 'orange';
+    //console.debug('letra %s', letra);
+
+    contentLetras.style.pointerEvents = "none";
+
+    var audio = new Audio('../sounds/' + letra + '.wav');
+    audio.play();
+    audio.onended = function(){
+        contentLetras.style.pointerEvents = "auto";
+    }
+
 }

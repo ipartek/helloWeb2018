9c10c9eebca31dd15316b1f164ac0e7891dc5d62
Curso <Curso@IPARTEKAULA.COM>
2018-02-21 11:10:37 +0100

    flexbox- con errores

---
+++ js/flexbox_solucion_pointerEvent.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var contentLetras = document.getElementById("container-flex");
+
+var boxes = document.getElementsByClassName("box2");
+for (var i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('click', sonido);
+}
+
+function sonido() {
+    var letra = event.target.parentElement.dataset.letter;
+    event.target.parentElement.style.backgroundColor = 'orange';
+    console.debug('letra %s', letra);
+
+    contentLetras.style.pointerEvents = "none";
+
+    var audio = new Audio('../sounds/' + letra + '.wav');
+    audio.play();
+    audio.onended = function () {
+        contentLetras.style.pointerEvents = "auto";
+    }
+
+}

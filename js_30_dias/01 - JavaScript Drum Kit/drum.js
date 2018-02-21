e9556cc19a628d53266717587053a3d1471875f7
Curso <Curso@IPARTEKAULA.COM>
2018-02-21 14:27:44 +0100

    clase 21 febrero 2018

---
+++ js_30_dias/01 - JavaScript Drum Kit/drum.js
@@ -0,0 +1,42 @@
+ //sonidos
+
+ //pointerEvent en un div los eventos de ratón no funcionan
+
+ var teclas = document.getElementById('keys');
+
+ //key down te devuelve siempre mayúscula
+ window.addEventListener('keydown', sonido);
+
+
+ function sonido() {
+
+     //console.log('tecla pulsada %o', event);
+
+     //coge la tecla
+     var tecla = event.keyCode;
+
+     var audio = document.querySelector('audio[data-key="' + tecla + '"]');
+     audio.play();
+
+
+     var aTecla = document.querySelectorAll('.key');
+     var teclaSel;
+
+     for (var i = 0; i < aTecla.length; i++) {
+         if (tecla == aTecla[i].getAttribute("data-key")) {
+
+             teclaSel= aTecla[i];
+             teclaSel.classList.add("playing");
+
+             audio.onended = function () {
+
+                 teclaSel.classList.remove("playing");
+            }
+
+         }
+
+
+     }
+
+
+ }

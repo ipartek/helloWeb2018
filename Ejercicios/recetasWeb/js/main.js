74a567fa7373b3703a8b9d09eb3df5e9eed9ad5b
Curso <Curso@IPARTEKAULA.COM>
2018-02-23 09:14:39 +0100

    23f1

---
+++ Ejercicios/recetasWeb/js/main.js
@@ -0,0 +1,22 @@
+
+var recetas = [];
+
+/* Creamos unas recetas de prueba */
+
+function init(){
+
+    var rPollo = new Receta("Pollo", "https://www.petitchef.es/imgupl/recipe/pollo-al-chilindron-con-verduras--md-100954p148148.jpg", 13, "Robin Food");
+
+    rPollo.addIngrediente("pollo");
+    rPollo.addIngrediente("ajo");
+    rPollo.addIngrediente("tomate");
+    rPollo.addIngrediente("chilindron");
+
+    console.log("1º receta %o", rPollo);
+
+
+    // Crear mas recetas
+
+    // Añadirlas al array recetas
+
+}

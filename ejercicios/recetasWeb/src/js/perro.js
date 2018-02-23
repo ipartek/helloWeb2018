7f6c90024c5dddcc33bd7131e6a4121319c2a4fe
alexanderDevWeb-C <dev.alexander.web@gmail.com>
2018-02-23 14:30:50 +0100

    recetas_cocina_gulp

---
+++ ejercicios/recetasWeb/src/js/perro.js
@@ -0,0 +1,15 @@
+class Perro{
+
+    constructor(){
+        this.nombre = "Tobi";
+        this.raza = "Husky";
+        this.chip = "9876";
+        this.imagen = "http://www.teleamazonas.com/wp-content/uploads/2017/11/perro.jpg";
+    }
+
+    ladra(){
+        console.log("Guau guau, grrrrrr")
+    }
+}
+
+//new Perro("a", "aa").ladra();

98e22cc8dd7e1e83523ef15ed170911d9d651c92
Curso <Curso@IPARTEKAULA.COM>
2018-02-23 09:16:59 +0100

    Ejercicio de examen 23-02-18

---
+++ ejercicios/recetas_web/js/main.js
@@ -0,0 +1,24 @@
+
+
+var recetas=[];
+
+/* creamos unas recetas de prueba */
+function init(){
+
+    var rPollo=new Receta("Pollo","",13,"Robin Food");
+
+    rPollo.addIngrediente("pollo");
+    rPollo.addIngrediente("ajo");
+    rPollo.addIngrediente("tomate");
+    rPollo.addIngrediente("chilindron");
+
+
+    console.log("1ª receta %o", rPollo);
+
+
+    //crear más recetas
+
+
+    //añadirlas al array recetas
+
+}
---
+++ ejercicios/recetas_web/js/receta.js
@@ -0,0 +1,19 @@
+class Receta{
+
+    constructor(nombre, foto, likes, cocinero){
+
+        this.nombre=nombre;
+        this.foto=foto;
+        this.likes=likes;
+        this.cocinero=cocinero;
+
+        this.ingredientes=[];
+    }
+
+
+    addIngrediente(ingrediente){
+        this.ingredientes.push(ingrediente);
+    }
+
+
+}

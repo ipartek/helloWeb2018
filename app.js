e1dc44bab9f6148536ea4e7a06d9b6aff465bb61
anderuraga <ander.uraga@gmail.com>
2018-02-23 09:15:51 +0100

    rectas web

--- app.js
+++ app.js
@@ -1,31 +1,61 @@
 /* Ejemplo para levantar un servidor web con NODEJS */
 
-//importar librerias
-var http = require('http');
-var url  = require('url');
+var express = require("express"),
+    app = express(),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override");
+    mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var router = express.Router();
+
+var recetas = [{
+  "nombre" : "Pollo a la cerverza",
+  "dificultad" : "Baja",
+  "duracion" : "2 horas",
+  "ingredientes" : ["pollo", "cerveza", "sal", "pimienta", "cebolla"],
+  "categoria" : "carnes",
+  "autor" : {
+    "nombre" : "Carlos Arguiñano",
+    "restaurante" : "el suyo",
+    "estrellas" : 1
+  	}
+	},
+  {
+    "nombre" : "Sorbete de limón",
+    "dificultad" : "Baja",
+    "duracion" : "0.5 horas",
+    "ingredientes" : ["4 limones", "1 litro de agua", "1 kg de azúcar", "hielo"],
+    "categoria" : "postres",
+    "autor" : {
+      "nombre" : "Juan Mari Arzak",
+      "restaurante" : "sorbetilandia",
+      "estrellas" : 1
+      }
+    }
+]
+
+router.get('/receta/', function(req, res) {
+   res.status(200).json(recetas);
+});
+
+router.get('/receta/:id', function(req, res) {
+
+   var id = req.params.id;
+   console.log(`receta = ${id}`);
+   let receta = recetas[id];
+
+   res.status(200).json(receta );
+});
 
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
 
-    console.log("request %o",request);
 
-    //recibir paramatros por GET
-    //ejemplo de uso: http://localhost:8084/?nombre=Ander&apellido=Uraga&edad=37
-    var parametros = url.parse( request.url, true ).query;
+app.use(router);
 
-    //TODO validar parametros
-
-    //Respuesta para el navegador
-    response.write('<h1>Servidor escuhando</h1>');
-    response.write('<p>Nombre: '+parametros.nombre+'</p>');
-    response.write('<p>Apellido:'+parametros.apellido+'</p>');
-    response.write('<p>Edad:'+parametros.edad+'</p>');
-
-    //termina response
-    response.end();
-
-}).listen(8084);
-
-// para probarlo abrir navegador y pegar:
-// localhost:8084
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+});
---
+++ ejercicios/recetas_web/js/main.js
@@ -0,0 +1,22 @@
+
+var recetas = [];
+
+/* creamos unas recetas de prueba */
+function init(){
+
+    var rPollo = new Receta("Pollo","http://recetasdecocina.elmundo.es/wp-content/uploads/2016/10/receta-pollo-al-chilindron.jpg",13,"Robin Food");
+
+    rPollo.addIngrediente("pollo");
+    rPollo.addIngrediente("ajo");
+    rPollo.addIngrediente("tomate");
+    rPollo.addIngrediente("chilidron");
+
+
+    console.log("1º receta %s %o", rPollo.nombre , rPollo );
+
+
+    //crear mas recetas
+
+    //añadirlas al array recetas
+
+}

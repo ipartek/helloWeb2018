49cffcb7fc6fbda18092eb68964cbbdc3e0e248a
DavidViko <davidvs83@hotmail.com>
2018-02-25 19:33:44 +0100

    RecetasWeb con ingredientes añadibles

--- ejercicios/recetasWeb/js/main.js
+++ ejercicios/recetasWeb/js/main.js
@@ -1,8 +1,8 @@
 var recetas = [];
 
-var dinamico = `<div id="receta" class="col-xs-12 col-sm-6 col-md-4">
+var dinamico = `<div id="receta" class="recCompleta col-xs-12 col-sm-6 col-md-4">
                     <div class="thumbnail">
-                        <span onclick="borrarReceta(##POSICION##)" class="close">X</span>
+                        <span onclick="borrarReceta(##POSICION##)" class="close">&times;</span>
                         <img id="foto" src="##FOTO##" alt="foto">
                         <div class="caption">
                             <h3 id="tituloReceta">##NOMBRE##</h3>
@@ -10,11 +10,11 @@ var dinamico = `<div id="receta" class="col-xs-12 col-sm-6 col-md-4">
                                 <span id="likes"><i class="fa fa-heart text-danger" aria-hidden="true"></i>##LIKES##</span>
                                 <span id="cocinero">##COCINERO##</span>
                             </div>
-                            <p><a href="#" class="btn btn-primary btn-block" role="button" data-toggle="modal" data-target="#modal-receta">Ingredientes</a></p>
+                            <p><a href="#" class="btn btn-primary btn-block" onclick="pintarIngredientes(##POSICION##)" role="button" data-toggle="modal" data-target="#modal-receta##POSICION##"><i class="fa fa-eye" aria-hidden="true"></i> Ingredientes</a></p>
                         </div>
 
                         <!-- Modal video Receta-->
-                        <div class="modal fade" id="modal-receta" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                        <div class="modal fade" id="modal-receta##POSICION##" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                     <div class="modal-header">
@@ -25,7 +25,11 @@ var dinamico = `<div id="receta" class="col-xs-12 col-sm-6 col-md-4">
                                     </div>
                                     <div class="modal-body">
                                         <!-- 4:3 aspect ratio -->
-                                        <div class="embed-responsive embed-responsive-4by3"></div>
+                                        <div class="embed-responsive embed-responsive-4by3">
+                                            <button type="button" class="btn btn-default" onclick="añadirIngrediente(##POSICION##)"><i class="fa fa-plus-square fa-2x" aria-hidden="true"></i></button> <input id="nuevoIngre##POSICION##" type="text" name="ingrediente" required placeholder="Añadir ingrediente">
+                                            <div id="cntIngredientes##POSICION##" class="cntIngredientes"></div>
+
+                                        </div>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -48,7 +52,6 @@ function init() {
     rPollo.addIngrediente("tomate");
     rPollo.addIngrediente("chilindrón");
 
-    console.log("1º receta %o", rPollo);
     //crear mas recetas
 
     var rSalmon = new Receta("Salmón", "https://t2.rg.ltmcdn.com/es/images/1/7/4/img_salmon_al_cilantro_34471_600.jpg", 25, "Arzak");
@@ -56,16 +59,11 @@ function init() {
     rSalmon.addIngrediente("perejil");
     rSalmon.addIngrediente("limon");
 
-    console.log("2º receta %o", rSalmon);
-
-
     var rAlubias = new Receta("Alubias", "http://1.bp.blogspot.com/_YsHFEIDj5bg/SQy83zgLajI/AAAAAAAAAoE/k9ccaecoMrQ/s640/019.JPG", 18, "Arguiñano");
-    rAlubias.addIngrediente("abubia");
+    rAlubias.addIngrediente("alubias");
     rAlubias.addIngrediente("chorizo");
     rAlubias.addIngrediente("morcilla");
     rAlubias.addIngrediente("tocino");
-
-    console.log("3 receta %o", rAlubias);
 
     var rPaella = new Receta("Paella", "https://www.comedera.com/wp-content/uploads/2015/06/receta-paella-valenciana-1.jpg", 9, "Berasategui");
     rPaella.addIngrediente("arroz");
@@ -73,31 +71,28 @@ function init() {
     rPaella.addIngrediente("azafran");
     rPaella.addIngrediente("langostino");
 
-    console.log("4º receta %o", rPaella);
-
     // añadirlas al array de recetas
 
     recetas.push(rPollo);
     recetas.push(rSalmon);
     recetas.push(rAlubias);
     recetas.push(rPaella);
-    pintarTodos();
+    pintarRecetas();
 }
 
-function pintarTodos() {
-    recetas.forEach(function (elem,index) {
-        pintarReceta(elem,index);
+function pintarRecetas() {
+    recetas.forEach(function (elem, index) {
+        pintarReceta(elem, index);
     });
 }
 
-function pintarReceta(elem,i) {
+function pintarReceta(elem, i) {
     var detalleReceta = dinamico;
-    detalleReceta = detalleReceta.replace('##POSICION##', i);
+    detalleReceta = detalleReceta.split('##POSICION##').join(i);
     detalleReceta = detalleReceta.replace('##NOMBRE##', elem.nombre);
     detalleReceta = detalleReceta.replace('##FOTO##', elem.foto);
     detalleReceta = detalleReceta.replace('##LIKES##', elem.likes);
     detalleReceta = detalleReceta.replace('##COCINERO##', elem.cocinero);
-    console.log(detalleReceta);
     cntRecetas.innerHTML = detalleReceta + cntRecetas.innerHTML;
 }
 
@@ -112,14 +107,36 @@ function añadirReceta() {
 
     recetas.push(receta);
 
-    pintarReceta(receta,recetas.length);
-    console.log(cntRecetas);
+    pintarReceta(receta, recetas.length);
 }
 
 function borrarReceta(pos) {
     console.log(recetas[pos]);
-    recetas.splice(pos,1);
+    recetas.splice(pos, 1);
     console.log(recetas);
     cntRecetas.innerHTML = "";
     pintarTodos();
+}
+
+function pintarIngredientes(pos) {
+    var cntIngredientes = document.getElementById("cntIngredientes" + pos);
+    cntIngredientes.innerHTML = "";
+    recetas[pos].ingredientes.forEach(function (ingre) {
+        pintarIngrediente(ingre, pos);
+    });
+}
+
+function pintarIngrediente(ingre, pos) {
+//    var cntIngredientes = document.getElementById("cntIngredientes" + pos);
+
+    var cntIngredientes= document.querySelector('div[id="cntIngredientes'+pos+'"]');
+    cntIngredientes.innerHTML += ingre + "<br>";
+}
+
+function añadirIngrediente(pos) {
+    var nuevoIngre = document.getElementById("nuevoIngre"+pos).value;
+
+    recetas[pos].addIngrediente(nuevoIngre);
+    console.log(recetas[pos].ingredientes);
+    pintarIngredientes(pos);
 }

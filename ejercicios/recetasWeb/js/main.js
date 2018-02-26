10edc61ddf3eb845cabd9ca5d74956bef1977262
Curso <Curso@IPARTEKAULA.COM>
2018-02-26 09:19:04 +0100

    Formulario validado

--- ejercicios/recetasWeb/js/main.js
+++ ejercicios/recetasWeb/js/main.js
@@ -1,24 +1,24 @@
-var paella=new Receta("Paella","https://www.goya.com/media/3961/shrimp-paella.jpg?quality=80",16,"Carlos Argiñano");
-var ensalada=new Receta("Ensalada","https://t1.rg.ltmcdn.com/es/images/3/2/6/img_ensalada_de_verduras_variadas_57623_600.jpg",6,"Alberto Chicote");
-var recetas=[];
+var paella = new Receta("Paella", "https://www.goya.com/media/3961/shrimp-paella.jpg?quality=80", 16, "Carlos Argiñano");
+var ensalada = new Receta("Ensalada", "https://t1.rg.ltmcdn.com/es/images/3/2/6/img_ensalada_de_verduras_variadas_57623_600.jpg", 6, "Alberto Chicote");
+var recetas = [];
 recetas.push(paella);
 recetas.push(ensalada);
 
 /* creamos unas recetas de prueba */
-function init(){
+function init() {
 
-    var idRecetas=document.getElementById("recetas");
-    var lis="";
-    var i=0;
-    recetas.forEach( receta => {
-        console.log("la receta %o",recetas[i])
+    var idRecetas = document.getElementById("recetas");
+    var lis = "";
+    var i = 0;
+    recetas.forEach(receta => {
+        console.log("la receta %o", recetas[i])
         lis += `<div class="col-xs-12 col-sm-6 col-md-4 detalleReceta">
-                <i class="fa fa-times-circle fa-2x" aria-hidden="true" onclick="eliminarReceta(`+i+`)"></i>
+                <i class="fa fa-times-circle fa-2x" aria-hidden="true" onclick="eliminarReceta(` + i + `)"></i>
                 <div class="thumbnail">
-                    <img src="`+recetas[i].foto+`">
+                    <img src="` + recetas[i].foto + `">
                     <div class="caption">
-                        <h2>`+recetas[i].nombre+`</h2><p>
-                        <i class="fa fa-heart likes" aria-hidden="true"></i><span class="likes">`+recetas[i].likes+`</span> <span class="cocinero">`+recetas[i].cocinero+`</span></p>
+                        <h2>` + recetas[i].nombre + `</h2><p>
+                        <i class="fa fa-heart likes" aria-hidden="true"></i><span class="likes">` + recetas[i].likes + `</span> <span class="cocinero">` + recetas[i].cocinero + `</span></p>
 
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal"><i class="fa fa-eye fa-2" aria-hidden="true"></i>
 Ver Recetas</button>
@@ -29,24 +29,33 @@ Ver Recetas</button>
             </div>`
         i++;
     });
-    idRecetas.innerHTML=lis;
+    idRecetas.innerHTML = lis;
 }
 
-function anadirReceta(){
-    var nombreR=document.getElementById("nombre").value;
-    var fotoR=document.getElementById("foto").value;
-    var likesR=document.getElementById("likes").value;
-    var cocineroR=document.getElementById("cocinero").value;
-    var receta= new Receta(nombreR,fotoR,likesR,cocineroR);
-    recetas.unshift(receta);
-    resetear();
-    init()
+function anadirReceta() {
+    //comprobar que el formulario es correcto//
+    var form = document.getElementById("formulario");
+    if (form.checkValidity()) {
+        console.log("Crear Receta");
+        var nombreR = document.getElementById("nombre").value;
+        var fotoR = document.getElementById("foto").value;
+        var likesR = document.getElementById("likes").value;
+        var cocineroR = document.getElementById("cocinero").value;
+        var receta = new Receta(nombreR, fotoR, likesR, cocineroR);
+        recetas.unshift(receta);
+        resetear();
+        init()
+    } else {
+        console.warn("no se puede crear porque el formulario no es correcto")
+    }
+
 
 
 }
-function eliminarReceta(pPosicion){
-    for(var i=pPosicion;i<recetas.length-1;i++){
-        recetas[i]=recetas[i+1];
+
+function eliminarReceta(pPosicion) {
+    for (var i = pPosicion; i < recetas.length - 1; i++) {
+        recetas[i] = recetas[i + 1];
     }
 
     recetas.pop();
@@ -54,16 +63,19 @@ function eliminarReceta(pPosicion){
     init()
 
 }
-function resetear(){
-    document.getElementById("nombre").value="";
-    document.getElementById("foto").value="";
-    document.getElementById("likes").value="";
-    document.getElementById("cocinero").value="";
+
+function resetear() {
+    document.getElementById("nombre").value = "";
+    document.getElementById("foto").value = "";
+    document.getElementById("likes").value = "";
+    document.getElementById("cocinero").value = "";
 
 }
-function cambiarDestino(){
-    document.getElementById("cambiante").innerHTML=`<a href="#" onclick="cambiarOtraVez()">Anadir nueva receta</a>`
+
+function cambiarDestino() {
+    document.getElementById("cambiante").innerHTML = `<a href="#" onclick="cambiarOtraVez()">Anadir nueva receta</a>`
 }
-function cambiarOtraVez(){
-    document.getElementById("cambiante").innerHTML=`<a href="#desplegable" onclick="cambiarDestino()">Anadir nueva receta</a>`
+
+function cambiarOtraVez() {
+    document.getElementById("cambiante").innerHTML = `<a href="#desplegable" onclick="cambiarDestino()">Anadir nueva receta</a>`
 }

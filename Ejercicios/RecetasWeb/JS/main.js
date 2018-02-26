5df4e816c2d901e6f24635d02d72c9f9d202717e
Curso <Curso@IPARTEKAULA.COM>
2018-02-26 14:30:25 +0100

    Arreglarlo

--- Ejercicios/RecetasWeb/JS/main.js
+++ Ejercicios/RecetasWeb/JS/main.js
@@ -1,4 +1,5 @@
 /*Creamos unas recetas de pruebas*/
+"use strict";
 
 
 var recetas = [];
@@ -8,65 +9,128 @@ var recetas = [];
 
 function init() {
 
-    var rPollo = new Receta("Pollo", "https://img-global.cpcdn.com/002_recipes/bc40f95a58a933fb/400x400cq70/photo.jpg", 13, "RobinFood");
+    crearRecetasMock();
+
+    cargarRecetas();
+
+
+}
+
+
+
+
+function cargarRecetas() {
+
+    var recetasHtml = "";
+
+ var contenedorRecetas = document.getElementById("contenedor_recetas");
+
+    recetas.forEach((receta, index) => {
+
+        console.log("%i receta %o", index, receta);
+        recetasHtml += `<div class="col-sm-6 col-md-4 receta">
+
+<div class="cerrar"><span onclick="borrarCaja(this, ${index} )">X</span></div>
+                <div class="thumbnail text-center">
+                    <img src="` + receta.foto + `" alt="..." >
+                    <div class="caption">
+                        <h3>` + receta.nombre + `</h3>
+                        <p>` + receta.descripcion + `.</p>
+                        <i class="fa fa-heart" aria-hidden="true">` + receta.likes + `</i> ` + receta.cocinero + `
+                        <p><a href="#pollo" class="btn btn-primary" role="button" data-toggle="modal" data-target="#pollo"><i class="fa fa-eye" aria-hidden="true"></i> Ingredientes</a>
+                    </div>
+                </div>
+            </div>`
+    });
+    contenedorRecetas.innerHTML = recetasHtml;
+
+}
+function crearRecetasMock() {
+
+    var rPollo = new Receta("Pollo", "http://recetasdecocina.elmundo.es/wp-content/uploads/2016/10/receta-pollo-al-chilindron.jpg", 13, "Robin Food");
 
     rPollo.addingredientes("pollo");
     rPollo.addingredientes("ajo");
     rPollo.addingredientes("tomate");
-    rPollo.addingredientes("chilindron");
-
-
-    console.log("1º receta %o", rPollo);
-
-
+    rPollo.addingredientes("chilidron");
 
     //crear mas recetas
+    var tortillaPatatas = new Receta("Tortilla Patatas", "https://es.rc-cdn.community.thermomix.com/recipeimage/flz0tm0r-a2069-162740-cfcd2-zlr8kinc/733f35bb-e440-4c4f-9b87-949a0e20e384/main/tortilla-de-patatas-con-cebolla.jpg", 45, "Karlos Argiñano");
+    tortillaPatatas.addingredientes("Patatas");
+    tortillaPatatas.addingredientes("Huevos");
 
 
 
     //añadirlas al array recetas
-}
+    recetas.push(rPollo);
+    recetas.push(tortillaPatatas);
 
-function anadir_receta() {
-
-    var nombre = document.getElementById("nombre").value;
-    var foto = document.getElementById("foto").value;
-    var cocinero = document.getElementById("cocinero").value;
-    var likes = document.getElementById("likes").value;
-    var descripcion = document.getElementById("descripcion").value;
-
-    var contenedor = document.getElementById("contenedor_recetas");
-    contenedor.innerHTML = `<div class="columnas col-md-4">
-                <div class="thumbnail text-center">
-                    <img src="` + foto + `" alt="..." >
-                     <span onclick="borrarCaja(##posicion)" class="close">X</span>
-                    <div class="caption">
-                        <h3>` + nombre + `</h3>
-                        <p>` + descripcion + `.</p>
-                        <i class="fa fa-heart" aria-hidden="true">` + likes + `</i> ` + cocinero + `
-                        <p><a href="#pollo" class="btn btn-primary" role="button" data-toggle="modal" data-target="#pollo"><i class="fa fa-eye" aria-hidden="true"></i> Ingredientes</a>
-                    </div>
-                </div>
-            </div>` + contenedor.innerHTML;
-
-
-    var nuevaReceta = new Receta(nombre, foto, likes, cocinero);
-
-
-    nuevaReceta.addingredientes("bacalao");
-    nuevaReceta.addingredientes("ajo");
-    nuevaReceta.addingredientes("tomate");
-    nuevaReceta.addingredientes("patatas");
-  console.log("2º receta %o", nuevaReceta);
+    console.debug("Recetas Creadas");
 }
 
 
-function borrarCaja(){
-
-    var close=document.getElementsByClassName("close").checked = true;
 
 
-    close.innerHTML ="";
+
+
+
+function nuevaReceta() {
+    console.log("nuevaReceta click");
+
+    //comprobar que el formulario es correcto antes de crear
+    var form = document.getElementById("formulario");
+
+    if (form.checkValidity()) {
+
+        console.debug('crear receta');
+        //recoger values de los inputs
+        var nombre = document.getElementById("nombre").value;
+        var foto = document.getElementById("foto").value;
+        var likes = document.getElementById("likes").value;
+        var cocinero = document.getElementById("cocinero").value;
+        //crear receta nueva
+        var newReceta = new Receta(nombre, foto, likes, cocinero);
+
+        //añadir al array
+        recetas.unshift(newReceta);
+
+        //pintar nueva receta
+        cargarRecetas();
+
+        //limpiar campos del formulario
+        form.reset();
+
+    } else {
+        console.warn('No se puede crear porque form no correcto');
+    }
+
+}
+
+function borrarCaja(elem, posicion) {
+
+
+    console.log("eliminarReceta click posicion %i", posicion);
+
+    elem.parentElement.parentElement.style.display = 'none';
+    recetas.splice(posicion, 1);
+
+}
+
+
+
+function showModal(index) {
+
+    var recetaSeleccionada = recetas[index];
+    console.debug('showModal %o' + recetaSeleccionada);
+
+    $('#modalIngredientes').modal('show');
+
+    var ingredientes = recetaSeleccionada.ingredientes;
+    var lis = "";
+    ingredientes.forEach(ing => {
+        lis += "<li>" + ing + "</li>";
+    });
+    $("#listaIngredientes").html(lis);
 
 
 }
---
+++ Ejercicios/RecetasWeb/JS/mainander.js
@@ -0,0 +1,134 @@
+"use strict";
+
+var recetas = [];
+
+/* creamos unas recetas de prueba */
+function init(){
+
+    crearRecetasMock();
+
+    cargarRecetas();
+
+}
+
+/**
+* Mostramos Recetas por Pantalla
+*/
+function cargarRecetas(){
+
+
+    var contenedorRecetas = document.getElementById("containerRecetas");
+    contenedorRecetas.innerHTML = "";
+    var recetasHtml = "";
+
+    recetas.forEach( (receta, index) => {
+
+        console.log( "%i receta %o", index, receta );
+        recetasHtml += `<div class="col-sm-6 col-md-4 receta">
+                           <div class="cerrar"><span onclick="eliminarReceta(this, ${index} )">X</span></div>
+                            <div class="thumbnail">
+                                <img src="${receta.foto}" alt="${receta.nombre}">
+                                <div class="caption">
+                                    <h3>${receta.nombre}</h3>
+                                    <p>
+                                        <span class="likes"><i class="fa fa-heart" aria-hidden="true"></i>${receta.likes}</span>
+                                        <span class="cocinero">${receta.cocinero}</span></p>
+                                    <hr>
+                                    <p><a href="#" class="btn btn-primary" role="button" data-toggle="modal" onclick="showModal(${index})"><i class="fa fa-eye" aria-hidden="true"></i> Ingredientes</a></p>
+                                </div>
+                            </div>
+                        </div>`;
+
+    });
+
+    contenedorRecetas.innerHTML = recetasHtml;
+
+}
+
+
+/**
+*    Creamos varias recetas ficticias
+*/
+function crearRecetasMock(){
+
+    var rPollo = new Receta("Pollo","http://recetasdecocina.elmundo.es/wp-content/uploads/2016/10/receta-pollo-al-chilindron.jpg",13,"Robin Food");
+
+    rPollo.addIngrediente("pollo");
+    rPollo.addIngrediente("ajo");
+    rPollo.addIngrediente("tomate");
+    rPollo.addIngrediente("chilidron");
+
+    //crear mas recetas
+    var tortillaPatatas = new Receta("Tortilla Patatas","https://es.rc-cdn.community.thermomix.com/recipeimage/flz0tm0r-a2069-162740-cfcd2-zlr8kinc/733f35bb-e440-4c4f-9b87-949a0e20e384/main/tortilla-de-patatas-con-cebolla.jpg",45,"Karlos Argiñano");
+    tortillaPatatas.addIngrediente("Patatas");
+    tortillaPatatas.addIngrediente("Huevos");
+
+    //añadirlas al array recetas
+    recetas.push(rPollo);
+    recetas.push(tortillaPatatas);
+
+    console.debug("Recetas Creadas");
+}
+
+
+function nuevaReceta(){
+    console.log("nuevaReceta click");
+
+    //comprobar que el formulario es correcto antes de crear
+    var form = document.getElementById("formulario");
+    if ( form.checkValidity() ){
+        console.debug('crear receta');
+        //recoger values de los inputs
+        var nombre   = document.getElementById("nombre").value;
+        var foto     = document.getElementById("foto").value;
+        var likes    = document.getElementById("likes").value;
+        var cocinero = document.getElementById("cocinero").value;
+        //crear receta nueva
+        var newReceta = new Receta(nombre, foto, likes, cocinero);
+
+        //añadir al array
+        recetas.unshift(newReceta);
+
+        //pintar nueva receta
+        cargarRecetas();
+
+        //limpiar campos del formulario
+        form.reset();
+
+    }else{
+        console.warn('No se puede crear porque form no correcto');
+    }
+
+}
+
+
+function eliminarReceta(elem, posicion ){
+    console.log("eliminarReceta click posicion %i", posicion );
+
+    elem.parentElement.parentElement.style.display = 'none';
+    recetas.splice(posicion,1);
+
+
+
+}
+
+
+function showModal(index){
+
+    var recetaSeleccionada = recetas[index];
+    console.debug('showModal %o' + recetaSeleccionada );
+
+    $('#modalIngredientes').modal('show');
+
+    var ingredientes = recetaSeleccionada.ingredientes;
+    var lis = "";
+    ingredientes.forEach( ing => {
+            lis += "<li>"+ing+"</li>";
+    });
+    $("#listaIngredientes").html(lis);
+
+
+}
+
+
+

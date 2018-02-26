f1898899102f92eabd633639d04c2f78e705aabc
Jorge <j.alcauzar@outlook.es>
2018-02-26 22:27:30 +0100

    cambios

--- ejemplos/recetasWeb/js/main.js
+++ ejemplos/recetasWeb/js/main.js
@@ -2,55 +2,88 @@ var recetas =[];
 
 
 function init(){
-    var rPollo=new Receta("Pollo","https://www.google.es/imgres?imgurl=https%3A%2F%2Funareceta.com%2Fwp-content%2Fuploads%2F2016%2F10%2Fpollo-asado.jpg&imgrefurl=https%3A%2F%2Funareceta.com%2Fpollo-asado%2F&docid=BD8dzgR0R2pb7M&tbnid=galsdItGyLivIM%3A&vet=10ahUKEwjj5KCfybvZAhVE6xQKHd8NDuMQMwhPKAEwAQ..i&w=1000&h=671&bih=662&biw=1366&q=pollo%20imagenes&ved=0ahUKEwjj5KCfybvZAhVE6xQKHd8NDuMQMwhPKAEwAQ&iact=mrc&uact=8",13,"Robin Food");
-    rPollo.addIngrediente("pollo");
-    rPollo.addIngrediente("ajo");
-    rPollo.addIngrediente("tomate");
-    rPollo.addIngrediente("chilindron");
-   pintarReceta(rPollo);
+    crearRecetasMock();
 
-   // console.log("1º receta %o", rPollo);
+    cargarRecetas();
+
 
 }
+function crearRecetasMock(){
+     var rPolloCurry=new Receta("Pollo Curry","https://www.comedera.com/wp-content/uploads/2016/07/pollo-al-curry-receta.jpg",13,"Robin Food");
+    rPolloCurry.addIngrediente("pollo");
+    rPolloCurry.addIngrediente("ajo");
+    rPolloCurry.addIngrediente("tomate");
+    rPolloCurry.addIngrediente("jengibre de 2 cm");
 
-function pintarReceta(receta){
-    var contenedor = document.getElementById("contenedorRecetas");
+  // pintarReceta(rPollo);
 
-    contenedor.innerHTML = `<div class="container" id="contenedorRecetas >
-        <div class="row">
-            <div class="col-xs-12 col-md-4">
-                <span id="cerrar" onclick="eliminarReceta(event)">X</span>
+   // console.log("1º receta %o", rPollo);
+    var tortillaPatatas = new Receta("Tortilla Patatas","https://es.rc-cdn.community.thermomix.com/recipeimage/flz0tm0r-a2069-162740-cfcd2-zlr8kinc/733f35bb-e440-4c4f-9b87-949a0e20e384/main/tortilla-de-patatas-con-cebolla.jpg",45,"Karlos Argiñano");
+    tortillaPatatas.addIngrediente("Patatas");
+    tortillaPatatas.addIngrediente("Huevos");
+
+    //añadirlas al array recetas
+    recetas.push(rPolloCurry);
+    recetas.push(tortillaPatatas);
+}
+
+function cargarRecetas(){
+
+   var contenedorRecetas = document.getElementById("contenedorRecetas");
+
+
+    recetas.forEach( (receta, index) => {
+
+    contenedorRecetas.innerHTML = `<div class="col-xs-12 col-md-4">
+                <span id="cerrar"onclick="eliminarReceta(event)">X</span>
                 <div class="thumbnail">
 
-                    <img src=${receta.foto}...">
+                    <img src="${receta.foto}" alt="...">
                     <div class="caption">
                         <hr>
 
-                        <h3>${receta.nombre}</h3>
-                        <span><i class="fa fa-heart-o" aria-hidden="true"></i>  </span><span>${receta.likes}</span> <span>${receta.cocinero}</span>
+                        <h3>${receta.nombre} </h3>
+                        <span><i class="fa fa-heart-o" aria-hidden="true"></i>  </span><span id="likes">${receta.likes}</span> <span id="cocinero">${receta.cocinero}</span>
                         <hr>
 
-                        <a href="#" data-toggle="modal" data-target="#myModal1"><i class="fa fa-eye" aria-hidden="true"></i> ingredientes</a>
+                        <a href="#" data-toggle="modal" data-target="modal"onclick="showModal(${index})"><i class="fa fa-eye" aria-hidden="true"></i> ingredientes</a>
 
                     </div>
                 </div>
-            </div>` + contenedor.innerHTML;
+
+            </div>` + contenedorRecetas.innerHTML;
+});
 }
 
-function crear(){
-   var nombre= document.getElementById("inlineFormInputNombre").value;
-    console.log(nombre);
-   var foto= document.getElementById("inlineFormInputFoto").value;
-    var likes = document.getElementById("inlineFormInputLikes").value;
-    var cocinero= document.getElementById("inlineFormInputCocinero").value;
-    var rPollo1=new Receta(nombre, foto, likes, cocinero);
-     pintarReceta(rPollo1)
 
-
-}
 function eliminarReceta(e){
       var boxReceta=e.target.parentElement;
                 boxReceta.style.display="none";
 
 }
+
+function showModal(index){
+
+    var recetaSeleccionada = recetas[index];
+    console.debug('showModal %o' + recetaSeleccionada );
+
+    $('#modalIngredientes').modal('show');
+
+    var ingredientes = recetaSeleccionada.ingredientes;
+    var lis = "";
+    ingredientes.forEach( ing => {
+            lis += "<li>"+ing+"</li>";
+    });
+    $("#listaIngredientes").html(lis);
+
+
+}
+function showForm(){
+    document.getElementById("loginForms").style.display="block";
+}
+function hideForm(){
+       document.getElementById("loginForms").style.display="none";
+}
+
+
 
